8c2fa7cf6ac3856cb201d4ddc463176bd7e288b1
HediaBougi <hedia.bougi@gmail.com>
2020-09-02 15:03:56 +0200

    cleaning

--- app.py
+++ app.py
@@ -3,28 +3,13 @@ from flask import Flask, render_template, request, send_from_directory
 from keras_preprocessing import image
 from keras.models import load_model
 import numpy as np
-import tensorflow as tf
 
 app = Flask(__name__)
 
-# STATIC_FOLDER = 'static'
-# # Path to the folder where we'll store the upload before prediction
 UPLOAD_FOLDER = 'uploads'
-# # Path to the folder where we store the different models
+
 MODEL_FOLDER = 'models'
-#
-#
-# def load__model():
-#     """Load model once at running time for all the predictions"""
-#     print('[INFO] : Model loading ................')
-#     global model
-#     # model = tf.keras.models.load_model(MODEL_FOLDER + '/catsVSdogs.h5')
-#     model = load_model(MODEL_FOLDER + '/cat_dog_classifier.h5')
-#     global graph
-#     graph = tf.get_default_graph()
-#     print('[INFO] : Model loaded')
-#
-#
+
 def predict(fullpath):
     classifier = load_model('./models/catordog.h5')
     test_image = image.load_img(fullpath, target_size=(160, 160))
